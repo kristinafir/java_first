b9730ecbcd00d36f2eebddc2e162345876a807e4
kristinafir <kristinafir@mail.ru>
2024-04-09 13:14:45 +0300

    Создан пакет для моей первой программы

--- sandbox/src/main/java/prog.java
+++
@@ -1,7 +0,0 @@
-public class prog {
-
-public static void main(String[] args) {
-      System.out.println("Hallo, everybody");
-}
-}
-
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/prog.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class prog {
+
+public static void main(String[] args)
+{
+      System.out.println("Hello! Everybody!");
+}
+}
+
